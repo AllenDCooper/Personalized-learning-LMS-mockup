b0147b11bca57679f2187f3ed1474ee3e8893235
ADC04 <allendcooper@gmail.com>
2020-07-30 17:47:25 -0400

    cleaned code, state on app.js is no storing responses from modal self-assessment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,208 @@
-import React from 'react'
+import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import AccordionUnit from './Accordion/Accordion'
-import { Container, Jumbotron } from 'react-bootstrap'
+import { Container, Jumbotron, Accordion, Card } from 'react-bootstrap'
+import ModalAssessment from './ModalAssessment/ModalAssessment';
 
-function App() {
-  return (
-    <div>
-      <Container>
-        <Jumbotron>
-          <h1>App</h1>
-        </Jumbotron>
-        <section>
-          <AccordionUnit />
-        </section>
-      </Container>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    item1: null,
+    item2: null,
+    item3: null,
+    item4: null,
+    item5: null,
+    item6: null,
+    item7: null,
+    item8: null,
+    item9: null,
+    item10: null,
+  }
+
+  updateScore = (event) => {
+    const name = event.target.name;
+    const value = event.target.value
+
+    this.setState({
+      [name]: value
+    });
+
+    const answerStr = JSON.stringify(this.state)
+    console.log(`Updated state ${answerStr}`)
+  }
+
+  alertScore = () => {
+    alert(`Total Scores = ${JSON.stringify(this.state)}`)
+  }
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Jumbotron>
+            <h1>App</h1>
+          </Jumbotron>
+          <section>
+            <Accordion>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                  Self-Assessment (Initial)
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                  <div>
+                    <ModalAssessment updateScore={this.updateScore} alertScore={this.alertScore} />
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="1">
+                  Critical Thinking and Goal Setting
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="1">
+                  <div>
+                    <Card.Body>Activity 1.1</Card.Body>
+                    <Card.Body>Activity 1.2</Card.Body>
+                    <Card.Body>Activity 1.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="2">
+                  Motivation, Decision Making, and Personal Responsibility
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="2">
+                  <div>
+                    <Card.Body>Activity 2.1</Card.Body>
+                    <Card.Body>Activity 2.2</Card.Body>
+                    <Card.Body>Activity 2.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="3">
+                  Learning Preferences
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="3">
+                  <div>
+                    <Card.Body>Activity 3.1</Card.Body>
+                    <Card.Body>Activity 3.2</Card.Body>
+                    <Card.Body>Activity 3.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="4">
+                  Organization and Time Management
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="4">
+                  <div>
+                    <Card.Body>Activity 4.1</Card.Body>
+                    <Card.Body>Activity 4.2</Card.Body>
+                    <Card.Body>Activity 4.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="5">
+                  Reading
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="5">
+                  <div>
+                    <Card.Body>Activity 5.1</Card.Body>
+                    <Card.Body>Activity 5.2</Card.Body>
+                    <Card.Body>Activity 5.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="6">
+                  Note Taking
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="6">
+                  <div>
+                    <Card.Body>Activity 6.1</Card.Body>
+                    <Card.Body>Activity 6.2</Card.Body>
+                    <Card.Body>Activity 6.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="7">
+                  Memory and Studying
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="7">
+                  <div>
+                    <Card.Body>Activity 7.1</Card.Body>
+                    <Card.Body>Activity 7.2</Card.Body>
+                    <Card.Body>Activity 7.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="8">
+                  Test Taking
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="8">
+                  <div>
+                    <Card.Body>Activity 8.1</Card.Body>
+                    <Card.Body>Activity 8.2</Card.Body>
+                    <Card.Body>Activity 8.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="9">
+                  Information Literacy and Communication
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="9">
+                  <div>
+                    <Card.Body>Activity 9.1</Card.Body>
+                    <Card.Body>Activity 9.2</Card.Body>
+                    <Card.Body>Activity 9.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="10">
+                  Connecting with Others
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="10">
+                  <div>
+                    <Card.Body>Activity 10.1</Card.Body>
+                    <Card.Body>Activity 10.2</Card.Body>
+                    <Card.Body>Activity 10.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="11">
+                  Personal and Financial Health
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="11">
+                  <div>
+                    <Card.Body>Activity 11.1</Card.Body>
+                    <Card.Body>Activity 11.2</Card.Body>
+                    <Card.Body>Activity 11.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="12">
+                  Academic and Career Planning
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="12">
+                  <div>
+                    <Card.Body>Activity 12.1</Card.Body>
+                    <Card.Body>Activity 12.2</Card.Body>
+                    <Card.Body>Activity 12.3</Card.Body>
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </section>
+        </Container>
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/ModalAssessment/ModalAssessment.js
+++ src/ModalAssessment/ModalAssessment.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
 import { Modal, Button, Card, Form } from 'react-bootstrap';
-import FormRadio from '../FormRadio/FormRadio'
 
 class ModalAssessment extends Component {
 
   state = {
     show: false,
     setShow: false,
-    item1: null,
-    item2: null,
-    item3: null,
-    item4: null,
-    item5: null,
-    item6: null,
-    item7: null,
-    item8: null,
-    item9: null,
-    item10: null,
   }
 
   handleClose = () => {
@@ -25,7 +14,7 @@ class ModalAssessment extends Component {
       setShow: false
     });
     console.log(this.state.setShow);
-    alert(`Total Scores = ${JSON.stringify(this.state)}`)
+    this.props.alertScore();
   };
 
   handleShow = () => {
@@ -36,19 +25,10 @@ class ModalAssessment extends Component {
     console.log(this.state.setShow);
   };
 
-  updateScore = (event) => {
-    const name = event.target.name;
-    const value = event.target.value
-
-    this.setState({
-      [name]: value
-    });
-
-    const answerStr = JSON.stringify(this.state)
-    console.log(`Updated state ${answerStr}`)
-  }
-
   render() {
+
+    var questionArr = ["I’m aware of my strengths and weaknesses as a student.", "I feel a sense of control over my academic performance.", "It’s important for me to understand how I learn.", "When I finish a reading, I take time to review what I’ve learned.", "I review my notes after class.", "I connect material I’m studying to things I already know.", "I’m comfortable with public speaking.", "I’m confident in my ability to decide among several career options.", "My grades are a reflection of my effort.", "I have preferences when it comes to thinking about and organizing information."];
+
     return (
       <div>
         <Card variant="primary" onClick={this.handleShow}>
@@ -63,7 +43,71 @@ class ModalAssessment extends Component {
           </Modal.Header>
           <Modal.Body>
             <Form>
-              <FormRadio updateScore={this.updateScore} />
+              {questionArr.map((item, index) => (
+                <div key={`item-${index + 1}`} style={{ marginBottom: "40px" }}>
+                  <h6>{item}</h6>
+                  <Form.Check
+                    custom
+                    inline
+                    label="Strongly Agree"
+                    type="radio"
+                    id={`item-1-${index + 1}`}
+                    name={`item${index + 1}`}
+                    value="6"
+                    onChange={this.props.updateScore}
+                  />
+                  <Form.Check
+                    custom
+                    inline
+                    label="Agree"
+                    type="radio"
+                    id={`item-2-${index + 1}`}
+                    name={`item${index + 1}`}
+                    value="5"
+                    onChange={this.props.updateScore}
+                  />
+                  <Form.Check
+                    custom
+                    inline
+                    label="Somewhat Agree"
+                    type="radio"
+                    id={`item-3-${index + 1}`}
+                    name={`item${index + 1}`}
+                    value="4"
+                    onChange={this.props.updateScore}
+                  />
+                  <Form.Check
+                    custom
+                    inline
+                    label="Somewhat Disagree"
+                    type="radio"
+                    id={`item-4-${index + 1}`}
+                    name={`item${index + 1}`}
+                    value="3"
+                    onChange={this.props.updateScore}
+                  />
+                  <Form.Check
+                    custom
+                    inline
+                    label="Disagree"
+                    type="radio"
+                    id={`item-5-${index + 1}`}
+                    name={`item${index + 1}`}
+                    value="2"
+                    onChange={this.props.updateScore}
+                  />
+                  <Form.Check
+                    custom
+                    inline
+                    label="Strongly Disagree"
+                    type="radio"
+                    id={`item-6-${index + 1}`}
+                    name={`item${index + 1}`}
+                    value="1"
+                    onChange={this.props.updateScore}
+                  />
+                </div>
+              ))}
             </Form>
           </Modal.Body>
           <Modal.Footer>
