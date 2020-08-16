996a10e234229fd44ff45823abf822b75b681be0
ADC04 <allendcooper@gmail.com>
2020-08-16 11:16:01 -0400

    debugged random score, which was adding indexes instead of scores

--- src/App.js
+++ src/App.js
@@ -50,7 +50,10 @@ class App extends Component {
     this.setState(state => {
       const answerArr = []
       for (let i = 0; i < itemsArr.length; i++) {
-        answerArr.push(Math.floor(Math.random() * 6) + 1)
+        const randomAnswer = Math.floor(Math.random() * 6) + 1
+        console.log(randomAnswer)
+        answerArr.push(randomAnswer)
+        console.log(answerArr)
       }
       return {
         answerArr,
@@ -75,43 +78,63 @@ class App extends Component {
 
     console.log(`answerArr[0]: ${this.state.answerArr[0]}`)
     console.log(`type of answerArr[0]: ${typeof this.state.answerArr[0]}`)
+
+    let rawScoreArr = []
+
+    scales.forEach(element => {
+      console.log(element)
+      const scaleName = element.name
+      let rawScoreSum = 0
+      element.itemIndexes.forEach(index => {
+        const score = this.state.answerArr[index]
+        console.log(score)
+        rawScoreSum += score
+        console.log(rawScoreSum)
+      })
+      const scoreObj = {[scaleName]: rawScoreSum}
+      rawScoreArr.push(scoreObj)
+    })
+    console.log(rawScoreArr);;
+  
     // sum individual item scores to create aggregate raw scores for each scale
-    const criticalThinkingRawScore = this.state.answerArr[0] + this.state.answerArr[34] + this.state.answerArr[42] + this.state.answerArr[59] + this.state.answerArr[68] + this.state.answerArr[76]
+    // const criticalThinkingRawScore = this.state.answerArr[0] + this.state.answerArr[34] + this.state.answerArr[42] + this.state.answerArr[59] + this.state.answerArr[68] + this.state.answerArr[76]
 
-    const motivationRawScore = this.state.answerArr[1] + this.state.answerArr[8] + this.state.answerArr[16] + this.state.answerArr[27] + this.state.answerArr[43] + this.state.answerArr[60] + this.state.answerArr[69] + this.state.answerArr[77]
+    // const motivationRawScore = this.state.answerArr[1] + this.state.answerArr[8] + this.state.answerArr[16] + this.state.answerArr[27] + this.state.answerArr[43] + this.state.answerArr[60] + this.state.answerArr[69] + this.state.answerArr[77]
 
-    const learningRawScore = this.state.answerArr[2] + this.state.answerArr[9] + this.state.answerArr[21] + this.state.answerArr[28] + this.state.answerArr[35] + this.state.answerArr[44] + this.state.answerArr[51] + this.state.answerArr[61] + this.state.answerArr[78]
+    // const learningRawScore = this.state.answerArr[2] + this.state.answerArr[9] + this.state.answerArr[21] + this.state.answerArr[28] + this.state.answerArr[35] + this.state.answerArr[44] + this.state.answerArr[51] + this.state.answerArr[61] + this.state.answerArr[78]
 
-    const timeManagementRawScore = this.state.answerArr[10] + this.state.answerArr[22] + this.state.answerArr[29] + this.state.answerArr[45] + this.state.answerArr[52] + this.state.answerArr[62] + this.state.answerArr[70]
+    // const timeManagementRawScore = this.state.answerArr[10] + this.state.answerArr[22] + this.state.answerArr[29] + this.state.answerArr[45] + this.state.answerArr[52] + this.state.answerArr[62] + this.state.answerArr[70]
 
-    const readingRawScore = this.state.answerArr[3] + this.state.answerArr[11] + this.state.answerArr[36] + this.state.answerArr[46] + this.state.answerArr[53] + this.state.answerArr[71] + this.state.answerArr[79]
+    // const readingRawScore = this.state.answerArr[3] + this.state.answerArr[11] + this.state.answerArr[36] + this.state.answerArr[46] + this.state.answerArr[53] + this.state.answerArr[71] + this.state.answerArr[79]
 
-    const noteTakingRawScore = this.state.answerArr[4] + this.state.answerArr[17] + this.state.answerArr[23] + this.state.answerArr[30] + this.state.answerArr[37] + this.state.answerArr[54] + this.state.answerArr[63]
+    // const noteTakingRawScore = this.state.answerArr[4] + this.state.answerArr[17] + this.state.answerArr[23] + this.state.answerArr[30] + this.state.answerArr[37] + this.state.answerArr[54] + this.state.answerArr[63]
 
-    const memoryRawScore = this.state.answerArr[5] + this.state.answerArr[18] + this.state.answerArr[38] + this.state.answerArr[55] + this.state.answerArr[64] + this.state.answerArr[80]
+    // const memoryRawScore = this.state.answerArr[5] + this.state.answerArr[18] + this.state.answerArr[38] + this.state.answerArr[55] + this.state.answerArr[64] + this.state.answerArr[80]
 
-    const testTakingRawScore = this.state.answerArr[12] + this.state.answerArr[24] + this.state.answerArr[39] + this.state.answerArr[47] + this.state.answerArr[65] + this.state.answerArr[72] + this.state.answerArr[81]
+    // const testTakingRawScore = this.state.answerArr[12] + this.state.answerArr[24] + this.state.answerArr[39] + this.state.answerArr[47] + this.state.answerArr[65] + this.state.answerArr[72] + this.state.answerArr[81]
 
-    const commRawScore = this.state.answerArr[6] + this.state.answerArr[19] + this.state.answerArr[25] + this.state.answerArr[48] + this.state.answerArr[66] + this.state.answerArr[73] + this.state.answerArr[82]
+    // const commRawScore = this.state.answerArr[6] + this.state.answerArr[19] + this.state.answerArr[25] + this.state.answerArr[48] + this.state.answerArr[66] + this.state.answerArr[73] + this.state.answerArr[82]
 
-    const connectingRawScore = this.state.answerArr[13] + this.state.answerArr[31] + this.state.answerArr[40] + this.state.answerArr[49] + this.state.answerArr[56] + this.state.answerArr[74] + this.state.answerArr[83]
+    // const connectingRawScore = this.state.answerArr[13] + this.state.answerArr[31] + this.state.answerArr[40] + this.state.answerArr[49] + this.state.answerArr[56] + this.state.answerArr[74] + this.state.answerArr[83]
 
-    const healthRawScore = this.state.answerArr[14] + this.state.answerArr[26] + this.state.answerArr[32] + this.state.answerArr[41] + this.state.answerArr[57] + this.state.answerArr[67] + this.state.answerArr[84]
+    // const healthRawScore = this.state.answerArr[14] + this.state.answerArr[26] + this.state.answerArr[32] + this.state.answerArr[41] + this.state.answerArr[57] + this.state.answerArr[67] + this.state.answerArr[84]
 
-    const planningRawScore = this.state.answerArr[7] + this.state.answerArr[15] + this.state.answerArr[20] + this.state.answerArr[33] + this.state.answerArr[50] + this.state.answerArr[58] + this.state.answerArr[75]
+    // const planningRawScore = this.state.answerArr[7] + this.state.answerArr[15] + this.state.answerArr[20] + this.state.answerArr[33] + this.state.answerArr[50] + this.state.answerArr[58] + this.state.answerArr[75]
 
-    const criticalThinkingScore = this.convertToPercentile(criticalThinkingRawScore, 1)
-    const motivationScore = this.convertToPercentile(motivationRawScore, 2);
-    const learningScore = this.convertToPercentile(learningRawScore, 3);
-    const timeManagementScore = this.convertToPercentile(timeManagementRawScore, 4);
-    const readingScore = this.convertToPercentile(readingRawScore, 5);
-    const noteTakingScore = this.convertToPercentile(noteTakingRawScore, 6);
-    const memoryScore = this.convertToPercentile(memoryRawScore, 7);
-    const testTakingScore = this.convertToPercentile(testTakingRawScore, 8);
-    const commScore = this.convertToPercentile(commRawScore, 9);
-    const connectingScore = this.convertToPercentile(connectingRawScore, 10);
-    const healthScore = this.convertToPercentile(healthRawScore, 11)
-    const planningScore = this.convertToPercentile(planningRawScore, 12);
+    const criticalThinkingScore = this.convertToPercentile(Object.values(rawScoreArr[0]), 1)
+    console.log(`criticalThinkingScore: ${criticalThinkingScore}`)
+    console.log(`object.values: ${Object.values(rawScoreArr[0])}`)
+    const motivationScore = this.convertToPercentile(Object.values(rawScoreArr[1]), 2);
+    const learningScore = this.convertToPercentile(Object.values(rawScoreArr[2]), 3);
+    const timeManagementScore = this.convertToPercentile(Object.values(rawScoreArr[3]), 4);
+    const readingScore = this.convertToPercentile(Object.values(rawScoreArr[4]), 5);
+    const noteTakingScore = this.convertToPercentile(Object.values(rawScoreArr[5]), 6);
+    const memoryScore = this.convertToPercentile(Object.values(rawScoreArr[6]), 7);
+    const testTakingScore = this.convertToPercentile(Object.values(rawScoreArr[7]), 8);
+    const commScore = this.convertToPercentile(Object.values(rawScoreArr[8]), 9);
+    const connectingScore = this.convertToPercentile(Object.values(rawScoreArr[9]), 10);
+    const healthScore = this.convertToPercentile(Object.values(rawScoreArr[10]), 11)
+    const planningScore = this.convertToPercentile(Object.values(rawScoreArr[11]), 12);
 
     // store the percentile scores for each scale into an array
     const percentileArr = [{ name: "criticalThinkingScore", value: criticalThinkingScore }, { name: "motivationScore", value: motivationScore }, { name: "learningScore", value: learningScore }, { name: "timeManagementScore", value: timeManagementScore }, { name: "readingScore", value: readingScore }, { name: "noteTakingScore", value: noteTakingScore }, { name: "memoryScore", value: memoryScore }, { name: "testTakingScore", value: testTakingScore }, { name: "commScore", value: commScore }, { name: "connectingScore", value: connectingScore }, { name: "healthScore", value: healthScore }, { name: "planningScore", value: planningScore }]
@@ -132,7 +155,6 @@ class App extends Component {
       goalTwo: goalTwo,
       goalThree: goalThree,
       scoreArr: [criticalThinkingScore, motivationScore, learningScore, timeManagementScore, readingScore, noteTakingScore, memoryScore, testTakingScore, commScore, connectingScore, healthScore, planningScore],
-      rawScoreArr: [criticalThinkingRawScore, motivationRawScore, learningRawScore, timeManagementRawScore, readingRawScore, noteTakingRawScore, memoryRawScore, testTakingRawScore, commRawScore, connectingRawScore, healthRawScore, planningRawScore]
     });
   }
 
