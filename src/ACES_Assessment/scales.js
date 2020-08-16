ca28f1ed4fcb80cfb6266ce9dc5a0bf4d0d22f29
ADC04 <allendcooper@gmail.com>
2020-08-16 12:45:41 -0400

    reformatted scales.js with line breaks

--- src/ACES_Assessment/scales.js
+++ src/ACES_Assessment/scales.js
@@ -1,5 +1,16 @@
 const scales = [
-  {name: "Critical Thinking and Goal Setting", itemIndexes: [0, 34, 42, 59, 68, 76]}, {name: "Motivation, Decision Making, and Personal Responsibility", itemIndexes: [1, 8, 16, 27, 43, 60, 69, 77]}, {name: "Learning Preferences", itemIndexes: [2, 9, 21, 28, 35, 44, 51, 61, 78]}, {name: "Organization and Time Management", itemIndexes: [10, 22, 29, 45, 52, 62, 70]}, {name: "Reading", itemIndexes: [3, 11, 36, 46, 53, 71, 79]}, {name: "Note Taking", itemIndexes: [4, 17, 23, 30, 37, 54, 63]}, {name: "Memory and Studying", itemIndexes: [5, 18, 38, 55, 64, 80]}, {name: "Test Taking", itemIndexes: [12, 24, 39, 47, 65, 72, 81]}, {name: "Information Literacy and Communication", itemIndexes: [6, 19, 25, 48, 66, 73, 82]}, {name: "Connecting with Others", itemIndexes: [13, 31, 40, 49, 56, 74, 83]}, {name: "Personal and Financial Health", itemIndexes: [14, 26, 32, 41, 57, 67, 84]}, {name: "Academic and Career Planning", itemIndexes: [7, 15, 20, 33, 50, 58, 75]}
+  { name: "Critical Thinking and Goal Setting", itemIndexes: [0, 34, 42, 59, 68, 76] },
+  { name: "Motivation, Decision Making, and Personal Responsibility",itemIndexes: [1, 8, 16, 27, 43, 60, 69, 77] },
+  { name: "Learning Preferences", itemIndexes: [2, 9, 21, 28, 35, 44, 51, 61, 78] },
+  { name: "Organization and Time Management", itemIndexes: [10, 22, 29, 45, 52, 62, 70] },
+  { name: "Reading", itemIndexes: [3, 11, 36, 46, 53, 71, 79] },
+  { name: "Note Taking", itemIndexes: [4, 17, 23, 30, 37, 54, 63] },
+  { name: "Memory and Studying", itemIndexes: [5, 18, 38, 55, 64, 80] },
+  { name: "Test Taking", itemIndexes: [12, 24, 39, 47, 65, 72, 81] },
+  { name: "Information Literacy and Communication", itemIndexes: [6, 19, 25, 48, 66, 73, 82] },
+  { name: "Connecting with Others", itemIndexes: [13, 31, 40, 49, 56, 74, 83] },
+  { name: "Personal and Financial Health", itemIndexes: [14, 26, 32, 41, 57, 67, 84] },
+  { name: "Academic and Career Planning", itemIndexes: [7, 15, 20, 33, 50, 58, 75] }
 ]
 
 export default scales;
