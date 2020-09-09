4933d92fcc17bfb1af765f5a4290c0294a00abe3
ADC04 <allendcooper@gmail.com>
2020-09-09 00:34:09 -0400

    added slider and formatting to accordion units

--- src/Components/ContentUnits/CommUnit.js
+++ src/Components/ContentUnits/CommUnit.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Accordion, Card } from 'react-bootstrap';
 import goals from '../../ACES_Assessment/goals';
 import './styles.css';
+import 'rc-tooltip/assets/bootstrap.css';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class CommUnit extends Component {
 
@@ -20,7 +23,7 @@ class CommUnit extends Component {
         if (this.state.show) {
           let goalString = "";
           goalString = goals[this.props.score.name][this.findValueTier(this.props.score.value)]
-          setTimeout(() => {this.typeWriter(goalString)}, 1000)
+          setTimeout(() => { this.typeWriter(goalString) }, 1000)
         } else {
           this.resetTypeWriter()
         }
@@ -58,11 +61,15 @@ class CommUnit extends Component {
           Information Literacy and Communication
       </Accordion.Toggle>
         <Accordion.Collapse eventKey="9">
-          <div>
-            <Card.Body><div style={{ fontSize: "18px" }}>{this.state.typed}<span className="cursor">|</span></div></Card.Body>
-            <Card.Body>Activity 1</Card.Body>
-            <Card.Body>Activity 2</Card.Body>
-            <Card.Body>Activity 3</Card.Body>
+          <div style={{ backgroundColor: "white" }}>
+            <Card.Body style={{ borderBottom: "1px solid #ededed", borderTop: "1px solid #ededed", paddingLeft: "2rem" }}><div style={{ fontSize: "18px", fontWeight: "500" }}>{this.state.typed}<span className="cursor"><span style={{ fontWeight: "normal" }}>|</span></span></div></Card.Body>
+            <Card.Body style={{ borderBottom: "1px solid #ededed", paddingLeft: "2rem" }}>Activity 1</Card.Body>
+            <Card.Body style={{ borderBottom: "1px solid #ededed", paddingLeft: "2rem" }}>Activity 2</Card.Body>
+            <Card.Body style={{ borderBottom: "1px solid #ededed", paddingLeft: "2rem" }}>Activity 3</Card.Body>
+            <Card.Body style={{ paddingLeft: "2rem", color: "gray" }}>
+              Goal completed?
+              <Slider style={{ width: "100%", margin: "20px 30px 20px 30px", maxWidth: "300px" }} min={0} max={1} defaultValue={0} marks={{ 0: "Not yet", 1: "Yes!", }} step={null} handleStyle={{ backgroundColor: "black", border: "solid 2px black" }} dotStyle={{ border: "solid 2px black" }} trackStyle={{ backgroundColor: "green" }} />
+            </Card.Body>
           </div>
         </Accordion.Collapse>
       </Card>
