9e56ad69a898077909b4054c58d72fd00f8070e1
ADC04 <allendcooper@gmail.com>
2020-10-28 16:54:30 -0400

    Updates

---
+++ precache-manifest.806701a65a66d3a2d963c1c164d04e25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b39c8ecf8cf0d8600348905bacf8339",
+    "url": "/aces-goalsetter-mockup/index.html"
+  },
+  {
+    "revision": "72c7c550b0e504817e80",
+    "url": "/aces-goalsetter-mockup/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b45f18a73412bb8c9026",
+    "url": "/aces-goalsetter-mockup/static/css/main.bf45612b.chunk.css"
+  },
+  {
+    "revision": "72c7c550b0e504817e80",
+    "url": "/aces-goalsetter-mockup/static/js/2.803437d1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/aces-goalsetter-mockup/static/js/2.803437d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45f18a73412bb8c9026",
+    "url": "/aces-goalsetter-mockup/static/js/main.609fbcf9.chunk.js"
+  },
+  {
+    "revision": "3baf874a0a4e6d154807",
+    "url": "/aces-goalsetter-mockup/static/js/runtime-main.8056f921.js"
+  }
+]);
