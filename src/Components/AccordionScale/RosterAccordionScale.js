18aabcec35e94180f61e98241963035ccf853f6f
ADC04 <allendcooper@gmail.com>
2021-01-20 23:14:34 -0500

    initial and progress roster reports working

---
+++ src/Components/AccordionScale/RosterAccordionScale.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Card, Accordion, ProgressBar, Row, Col, Button, Dropdown } from 'react-bootstrap';
+
+const RosterAccordionScale = (props) => {
+
+  return (
+    <Accordion>
+      <Card style={{ border: 'none' }}>
+        <Accordion.Toggle as={Button} variant="link" eventKey={props.index} style={{ width: '100%', boxShadow: 'none', padding: '0' }}>
+          <Row className={props.scoreTypeAlt ? 'comparison-progress-row' : null}>
+            <Col xs={12} md={4} style={window.innerWidth <= 740 ? props.styles.scaleTitleMobile : props.styles.scaleTitle}>
+              <span style={{ fontWeight: "400" }}>{props.userScore.userName}</span>
+            </Col>
+            <Col xs={12} md={8} style={{ margin: 'auto' }}>
+              <ProgressBar style={{ position: 'relative' }}>
+                <ProgressBar now={props.userScore.scores[props.index][props.scoreType]} label={`${props.userScore.scores[props.index][props.scoreType]}%`} style={props.userScore.scores[props.index][props.scoreType] < 0 ? { backgroundColor: 'red' } : {}} />
+              </ProgressBar>
+            </Col>
+            {props.scoreTypeAlt ?
+              <>
+                <Col xs={12} md={4} style={window.innerWidth <= 740 ? props.styles.scaleTitleMobile : props.styles.scaleTitle}>
+                </Col>
+                <Col xs={12} md={8} style={{ margin: 'auto' }}>
+                  <ProgressBar style={{ position: 'relative' }}>
+                    <ProgressBar animated now={props.userScore.scores[props.index][props.scoreTypeAlt]} label={`${props.userScore.scores[props.index][props.scoreType]}%`} />
+                  </ProgressBar>
+                </Col>
+              </>
+              :
+              null
+            }
+          </Row>
+        </Accordion.Toggle>
+      </Card>
+    </Accordion>
+  )
+}
+
+export default RosterAccordionScale;
--- src/Components/ReportsDashboard/RosterReport.js
+++ src/Components/ReportsDashboard/RosterReport.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Card, Accordion, ProgressBar, Row, Col, Button, Dropdown } from 'react-bootstrap';
 import './ClassReport.css';
 import scales from '../../ACES_Assessment/scales.js';
+import RosterAccordionScale from '../AccordionScale/RosterAccordionScale';
 
 function RosterReport(props) {
   console.log(props)
@@ -26,6 +27,68 @@ function RosterReport(props) {
     console.log(scaleShowName);
   }
 
+  // sort function that takes an array and will return new array with items in order from largest to smallest
+  const sortValuesDescending = (userArr, scaleIndex, scoreType) => {
+    userArr.sort(function (a, b) {
+      return b.scores[scaleIndex][scoreType] - a.scores[scaleIndex][scoreType]
+    })
+    let newArr = []
+    for (let i = 0; i < userArr.length; i++) {
+      newArr.push(userArr[i])
+    }
+    console.log(newArr)
+    return newArr
+  }
+
+  const sortStrengths = (arr, scaleIndex, scoreType, scoreTypeAlt) => {
+
+    const scoreTypeConfirmed = scoreTypeAlt ? scoreTypeAlt : scoreType
+    // sort the percentile array into descending order so that top strengths will appear first
+    const descendingArr = sortValuesDescending(arr, scaleIndex, scoreTypeConfirmed);
+    console.log(`descendingArr: ${descendingArr}`);
+
+    // instantiate an array to hold the 3 tiers of skill
+    const strengthsArr = [{ "Strengths": [] }, { "Developing_Strengths": [] }, { "Growth_Areas": [] }]
+
+    // map descending array, sorting out into 3 tiers and pushing into appropriate object in strengthsArr
+    descendingArr.forEach((userScore, index) => {
+      if (userScore.inClass) {
+        if (userScore.scores[scaleIndex][scoreTypeConfirmed] > 75) {
+          const arrCopy1 = strengthsArr[0].Strengths
+          let arr1 = []
+          arrCopy1 === undefined ? arr1 = [] : arr1 = [...arrCopy1]
+          arr1.push(userScore)
+          strengthsArr[0].Strengths = arr1
+        } else if (userScore.scores[scaleIndex][scoreTypeConfirmed] <= 75 && userScore.scores[scaleIndex][scoreTypeConfirmed] > 25) {
+          const arrCopy2 = strengthsArr[1].Developing_Strengths
+          let arr2 = []
+          arrCopy2 === undefined ? arr2 = [] : arr2 = [...arrCopy2]
+          arr2.push(userScore)
+          strengthsArr[1].Developing_Strengths = arr2
+        } else {
+          const arrCopy3 = strengthsArr[2].Growth_Areas
+          let arr3 = []
+          arrCopy3 === undefined ? arr3 = [] : arr3 = [...arrCopy3]
+          arr3.push(userScore)
+          strengthsArr[2].Growth_Areas = arr3
+        }
+      }
+    })
+    console.log(`strengthsArr: ${JSON.stringify(strengthsArr)}`)
+    return strengthsArr
+  }
+
+  const sortStrengthsByScale = () => {
+    let sortedStrengthsByScaleArr = []
+    scales.forEach((scale, scaleIndex) => {
+      sortedStrengthsByScaleArr.push(sortStrengths(props.userModelSeed, scaleIndex, props.scoreType, props.scoreTypeAlt))
+    })
+    return sortedStrengthsByScaleArr
+  }
+
+  const strengthsArrByScale = sortStrengthsByScale()
+  console.log(strengthsArrByScale)
+
   return (
     <div>
       <div className='report-instructions'>
@@ -43,43 +106,77 @@ function RosterReport(props) {
           </Dropdown.Menu>
         </Dropdown>
       </div>
-      {scales.map((scale, index) => (
-        props.userModelSeed.map(userScore => (
-          // <div>{userScore.scores[index].rawScoreInitial}</div>
-          userScore.inClass ?
-            <Accordion style={scale.name === scaleShowName ? { display: 'inherit' } : { display: 'none' }}>
-              <Card style={{ border: 'none' }}>
-                <Accordion.Toggle as={Button} variant="link" eventKey={index} style={{ width: '100%', boxShadow: 'none', padding: '0' }}>
-                  <Row className={props.scoreTypeAlt ? 'comparison-progress-row' : null}>
-                    <Col xs={12} md={4} style={window.innerWidth <= 740 ? styles.scaleTitleMobile : styles.scaleTitle}>
-                      <span style={{ fontWeight: "400" }}>{userScore.userName}</span>
-                    </Col>
-                    <Col xs={12} md={8} style={{ margin: 'auto' }}>
-                      <ProgressBar style={{ position: 'relative' }}>
-                        <ProgressBar now={userScore.scores[index][props.scoreType]} label={`${userScore.scores[index][props.scoreType]}%`} style={userScore.scores[index][props.scoreType] < 0 ? { backgroundColor: 'red' } : {}} />
-                      </ProgressBar>
-                    </Col>
-                    {props.scoreTypeAlt ?
-                      <>
-                        <Col xs={12} md={4} style={window.innerWidth <= 740 ? styles.scaleTitleMobile : styles.scaleTitle}>
-                        </Col>
-                        <Col xs={12} md={8} style={{ margin: 'auto' }}>
-                          <ProgressBar style={{ position: 'relative' }}>
-                            <ProgressBar animated now={userScore.scores[index][props.scoreTypeAlt]} label={`${userScore.scores[index][props.scoreType]}%`} />
-                          </ProgressBar>
-                        </Col>
-                      </>
-                      :
-                      null
-                    }
-                  </Row>
-                </Accordion.Toggle>
-              </Card>
-            </Accordion>
-            :
-            null
+      {props.strengthsViewOn ?
+        strengthsArrByScale.map((strengthsArr, scaleIndex) => (
+          <Accordion style={scales[scaleIndex].name === scaleShowName ? { display: 'inherit' } : { display: 'none' }}>
+            <Card style={styles.strengthsCard}>
+              <Card.Body style={styles.strengthsCardBody} >
+                <h5 className='strengths-header'>My {props.cohortName}'s Strengths</h5>
+                <div style={{ margin: '20px 0px' }}>
+                  {strengthsArr[0].Strengths.length === 0 ? <li>[empty]</li> : (
+                    strengthsArr[0].Strengths.map((userScore, index) => (
+                      <RosterAccordionScale scale={scales[scaleIndex]} index={scaleIndex} scoreType={props.scoreType} scaleShowName={scaleShowName} scoreTypeAlt={props.scoreTypeAlt} styles={styles} userScore={userScore} />
+                    ))
+                  )
+                  }
+                </div>
+                <h5 className='strengths-header'>My {props.cohortName}'s Developing Strengths</h5>
+                <div style={{ margin: '20px 0px' }}>
+                  {strengthsArr[1].Developing_Strengths.length === 0 ? <li>[empty]</li> : (
+                    strengthsArr[1].Developing_Strengths.map((userScore, index) => (
+                      <RosterAccordionScale scale={scales[scaleIndex]} index={scaleIndex} scoreType={props.scoreType} scaleShowName={scaleShowName} scoreTypeAlt={props.scoreTypeAlt} styles={styles} userScore={userScore} />
+                    ))
+                  )}
+                </div>
+                <h5 className='strengths-header'>My {props.cohortName}'s Growth Areas</h5>
+                <div style={{ margin: '20px 0px' }}>
+                  {strengthsArr[2].Growth_Areas.length === 0 ? <li>[empty]</li> : (
+                    strengthsArr[2].Growth_Areas.map((userScore, index) => (
+                      <RosterAccordionScale scale={scales[scaleIndex]} index={scaleIndex} scoreType={props.scoreType} scaleShowName={scaleShowName} scoreTypeAlt={props.scoreTypeAlt} styles={styles} userScore={userScore} />
+                    ))
+                  )}
+                </div>
+              </Card.Body>
+            </Card>
+          </Accordion>
         ))
-      ))}
+        :
+        scales.map((scale, index) => (
+          props.userModelSeed.map(userScore => (
+            userScore.inClass ?
+              <Accordion style={scale.name === scaleShowName ? { display: 'inherit' } : { display: 'none' }}>
+                <Card style={{ border: 'none' }}>
+                  <Accordion.Toggle as={Button} variant="link" eventKey={index} style={{ width: '100%', boxShadow: 'none', padding: '0' }}>
+                    <Row className={props.scoreTypeAlt ? 'comparison-progress-row' : null}>
+                      <Col xs={12} md={4} style={window.innerWidth <= 740 ? styles.scaleTitleMobile : styles.scaleTitle}>
+                        <span style={{ fontWeight: "400" }}>{userScore.userName}</span>
+                      </Col>
+                      <Col xs={12} md={8} style={{ margin: 'auto' }}>
+                        <ProgressBar style={{ position: 'relative' }}>
+                          <ProgressBar now={userScore.scores[index][props.scoreType]} label={`${userScore.scores[index][props.scoreType]}%`} style={userScore.scores[index][props.scoreType] < 0 ? { backgroundColor: 'red' } : {}} />
+                        </ProgressBar>
+                      </Col>
+                      {props.scoreTypeAlt ?
+                        <>
+                          <Col xs={12} md={4} style={window.innerWidth <= 740 ? styles.scaleTitleMobile : styles.scaleTitle}>
+                          </Col>
+                          <Col xs={12} md={8} style={{ margin: 'auto' }}>
+                            <ProgressBar style={{ position: 'relative' }}>
+                              <ProgressBar animated now={userScore.scores[index][props.scoreTypeAlt]} label={`${userScore.scores[index][props.scoreType]}%`} />
+                            </ProgressBar>
+                          </Col>
+                        </>
+                        :
+                        null
+                      }
+                    </Row>
+                  </Accordion.Toggle>
+                </Card>
+              </Accordion>
+              :
+              null
+          ))
+        ))}
     </div>
   )
 }
